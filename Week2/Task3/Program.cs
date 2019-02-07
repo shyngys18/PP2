1864601739db82e50db994577667cdcd89e63b3f
shyngys18 <chingiz100@gmail.com>
2019-02-07 11:29:40 +0600

    'upd'

---
+++ Week2/Task3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Shyngys\Desktop\Week1\Week2\Task1");
+            PrintInfo(dir, 0);
+        }
+        static void PrintInfo(FileSystemInfo fsi,int k)
+        {
+            string s = new string(' ', k);
+            Console.WriteLine(s + fsi.Name);
+
+            if (fsi.GetType() == typeof(DirectoryInfo))
+            {
+                FileSystemInfo[] arr = ((DirectoryInfo)fsi).GetFileSystemInfos();
+                for (int i = 0; i < arr.Length; ++i)
+                {
+                    PrintInfo(arr[i], k + 3);
+                }
+            }
+        }
+    }
+}
