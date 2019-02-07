5513e798822fae148407e5fdc60ef180d5176fa8
shyngys18 <chingiz100@gmail.com>
2019-02-07 12:06:35 +0600

    'hello'

---
+++ Week2/Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //DirectoryInfo path1 = new DirectoryInfo(@"C:\Users\Shyngys\Desktop\Week1\Week2\Task4\source");
+           // DirectoryInfo path2 = new DirectoryInfo(@"C:\Users\Shyngys\Desktop\Week1\Week2\Task4\destination");
+            string path = "C:\\Users\\Shyngys\\Desktop\\Week1\\Week2\\Task4\\source\\test.txt";
+            FileStream fs = File.Create(path);
+            fs.Close();
+            string destination = @"C:\Users\Shyngys\Desktop\Week1\Week2\Task4\destination\";
+
+
+
+
+
+            
+            File.Copy(path, destination + Path.GetFileName(path));
+            File.Delete(path);
+
+        }
+        
+    }
+}
